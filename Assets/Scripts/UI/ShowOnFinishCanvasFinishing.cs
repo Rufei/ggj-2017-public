cd54af5ee09f1f69e03916b9b75d73f54ff340a5
Jeffrey Hsu <silverhsu@gmail.com>
2017-02-15 01:20:50 -0600

    Added end of round Annoucner lines. Rebalanced announcer audio to 0.7

--- Assets/Scripts/UI/ShowOnFinishCanvasFinishing.cs
+++ Assets/Scripts/UI/ShowOnFinishCanvasFinishing.cs
@@ -16,6 +16,14 @@ public class ShowOnFinishCanvasFinishing : MonoBehaviour
     private bool isActivated = false;
     private bool isFinished = false;
 
+    public AudioClip player1Clip;
+    public AudioClip player2Clip;
+    public AudioClip drawClip;
+
+    private AudioSource matchOverAudioSource;
+
+    private bool hasAnnouncerPlayed = false;
+
     // Use this for initialization
     void Start()
     {
@@ -23,6 +31,8 @@ public class ShowOnFinishCanvasFinishing : MonoBehaviour
         canvas = GetComponent<Canvas>();
         canvas.enabled = false;
         gameData = GameObject.Find("/GameData").GetComponent<GameData>();
+
+        matchOverAudioSource = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -43,14 +53,32 @@ public class ShowOnFinishCanvasFinishing : MonoBehaviour
                 if (directorScript.scorePlayer1 > directorScript.scorePlayer2)
                 {
                     winText.text = "Player 1 Wins!";
+                    if (!hasAnnouncerPlayed)
+                    {
+                        hasAnnouncerPlayed = true;
+                        matchOverAudioSource.clip = player1Clip;
+                        matchOverAudioSource.Play();
+                    }
                 }
                 else if (directorScript.scorePlayer2 > directorScript.scorePlayer1)
                 {
                     winText.text = "Player 2 Wins!";
+                    if (!hasAnnouncerPlayed)
+                    {
+                        hasAnnouncerPlayed = true;
+                        matchOverAudioSource.clip = player2Clip;
+                        matchOverAudioSource.Play();
+                    }
                 }
                 else
                 {
                     winText.text = "It's a tie!";
+                        if (!hasAnnouncerPlayed)
+                        {
+                            hasAnnouncerPlayed = true;
+                            matchOverAudioSource.clip = drawClip;
+                            matchOverAudioSource.Play();
+                        }
                 }
                 if (Input.GetKeyDown(KeyCode.Escape))
                 {
